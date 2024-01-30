32cce30b8cedcc57560604d661a2ab6e730b8645
Mukarram Srour <mukarramsrour@gmail.com>
2024-01-30 19:56:57 +0300

    Logout & LogIn

--- src/magento/MyTestCases.java
+++ src/magento/MyTestCases.java
@@ -21,29 +21,50 @@ public class MyTestCases extends Parameters {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 	}
 
-	@Test
+	@Test(priority = 1)
 	public void SignUp() {
 		driver.get("https://magento.softwaretestingboard.com/");
 		driver.findElement(By.linkText("Create an Account")).click();
-		
+
 		WebElement firstNameInput = driver.findElement(By.id("firstname"));
 		WebElement lastNameInput = driver.findElement(By.id("lastname"));
 		WebElement passInput = driver.findElement(By.id("password"));
 		WebElement confirmPassInput = driver.findElement(By.id("password-confirmation"));
 		WebElement emailInput = driver.findElement(By.id("email_address"));
-		
+
 		WebElement createAccBtn = driver.findElement(By.cssSelector("button[title='Create an Account'] span"));
-		
+
 		firstNameInput.sendKeys(firstNames[rndIndex]);
 		lastNameInput.sendKeys(lastNames[rndIndex]);
 		passInput.sendKeys(commonPass);
 		confirmPassInput.sendKeys(commonPass);
 		emailInput.sendKeys(email);
-		
+
 		createAccBtn.click();
-		
+
 		String wlcMsg = driver.findElement(By.className("message-success")).getText();
-		
+
 		assertEquals(wlcMsg, "Thank you for registering with Main Website Store.");
+	}
+
+	@Test(priority = 2)
+	public void Logout() {
+		driver.get("https://magento.softwaretestingboard.com/customer/account/logout/");
+		assertEquals(driver.getCurrentUrl().contains("logoutSuccess"), true);
+	}
+
+	@Test(priority = 3)
+	public void SignIn() throws InterruptedException {
+
+		driver.findElement(By.linkText("Sign In")).click();
+		driver.findElement(By.id("email")).sendKeys(email);
+		driver.findElement(By.id("pass")).sendKeys(commonPass);
+
+		driver.findElement(By.id("send2")).click();
+
+		Thread.sleep(2000);
+	
+		assertEquals(driver.findElement(By.className("logged-in")).getText()
+				.contains("Welcome, " + firstNames[rndIndex] + " " + lastNames[rndIndex] + "!"), true);
 	}
 }
